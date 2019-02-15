9eef9e15bcb0967e38bd15909611bff38ff330d2
Retnan <dretnan@logicaladdress.com>
2019-02-15 14:48:50 +0100

    m

--- config/http.js
+++ config/http.js
@@ -10,6 +10,7 @@
  */
 
 var express = require('express');
+var serveStatic = require('serve-static')
 
 module.exports.http = {
 
@@ -81,10 +82,12 @@ module.exports.http = {
 
   },
 
-  customMiddleware: function (app) {
-    // this code will be executed only once during the application startup
-    app.use(express.static('assets'));
-    app.use(express.static('www'));
+  customMiddleware: function (req, res) {
+    console.log("we here");
+    var serve = serveStatic('assets', {
+      'index': false,
+      'setHeaders': setHeaders
+    })
+    serve(req, res)
   }
-
 };
